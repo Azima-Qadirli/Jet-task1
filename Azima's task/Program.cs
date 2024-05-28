1f3a87a9fa6dff6823f8236a8e1fe0801e2fefcb
Azima <azimaqadirli@gmail.com>
2024-05-28 18:34:57 +0400

    first task

---
+++ Azima's task/Program.cs
@@ -0,0 +1,78 @@
+﻿#region First task
+//Complete the function solveMeFirst to compute the sum of two integers.
+//int a: the first value
+//int b: the second value
+//Returns
+//- int: the sum of a  and b
+
+//int a = 5;
+//int b = 12;
+//int sum = a + b;
+//Console.WriteLine(sum);
+
+
+//Console.WriteLine("Enter your first number:");
+//int c = int.Parse(Console.ReadLine());
+//Console.WriteLine("Enter your second number:");
+//int d = int.Parse(Console.ReadLine());
+//int SUM = c + d;
+//Console.WriteLine(SUM);
+#endregion
+
+#region Second task
+//Given an array of integers, find the sum of its elements.
+//int[] arr = new int[6];
+//int[] arr1 = { 1, 2, 3, 4, 5, 6 };
+//int sum = arr1.Sum();
+//Console.WriteLine(sum);
+#endregion
+
+#region Third task
+//Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+//Console.WriteLine("Enter your number:");
+//int num = int.Parse(Console.ReadLine());
+//int result = 0;
+//while (num != 0)
+//{
+//    result = result * 10;
+//    result = result + num % 10;
+//    num = num / 10;
+//}
+//Console.WriteLine(result);
+
+//int number = 145;
+//int remainder, reversedNumber = 0;
+//while (number>0)
+//{
+//    remainder = number % 10;
+//    number = number / 10;
+//    reversedNumber = (reversedNumber * 10) + remainder;
+//}
+//Console.WriteLine("ReversedNumber is :"+ reversedNumber);
+#endregion
+
+#region Fourth task
+//Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order. We have to find index of target of array.
+
+//IndexOf  avtomatik cixdi  deye kodum isledi. Amma sorusacam yene dersde.
+
+//int[] arr = { 4, 5, 6, 7, 8, 9 };
+//int target = 9;
+//int index =Array.IndexOf(arr, target);
+//Console.WriteLine("Your target's index is:"+index);
+
+//int[] arr = { 4, 5, 6, 7, 8, 9 };
+//int target = 5;
+//int index = 0;
+
+//foreach (int num in arr)
+//{
+//    if (num == target)
+//    {
+//        Console.WriteLine("Your target's index is: " + index);
+        
+//    }
+//    index++;
+//}
+
+#endregion
